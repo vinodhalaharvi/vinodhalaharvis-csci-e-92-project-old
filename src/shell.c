736371f28fe0d2743820b6bd380bb9bae2eba0d2
Vinod Halaharvi <vinod.halaharvi@gmail.com>
2015-09-15 19:26:53 +0000

    changes ..

--- src/shell.c
+++ src/shell.c
@@ -3,9 +3,16 @@
 #include <string.h>
 #include <errno.h>
 #include <string.h>
+#include <assert.h>
+
+#define MAX_STRING_LENGTH 1000
 
 const int LINE_MAX = 256;
 typedef int (*command_func_type) (int argc, char * argv[]); 
+typedef enum {
+    false, 
+    true
+} boolean;
 
 int cmd_date(int argc, char *argv[]);
 int cmd_echo(int argc, char *argv[]);
@@ -51,14 +58,37 @@ int main(int argc, char *argv[]) {
     }
 }
 
+char *join(char * stringArray[], char * delimiter){ 
+    char * result = (char *) malloc(1000); 
+    char * base = result; 
+    int i;
+    memset(result, '\0', 1000); 
+    i = 0; 
+    while(stringArray != NULL && stringArray[i]){ 
+        strcpy(result, stringArray[i]); 
+        result += strlen(stringArray[i]); 
+        if (stringArray[i+1] != NULL) { 
+            strcpy(result, delimiter); 
+            result += strlen(delimiter); 
+        }
+        i++; 
+    }
+    return base;
+}
 
 int cmd_date(int argc, char *argv[]){ 
     fprintf(stdout, "%s\n", "cmd_date");
+    int time = gettimeofday(); 
+    if (time == 0){ 
+        fprintf(stdout, "Current time in epochs is: %d\n", time);
+    } else {  
+        perror("time"); 
+    }
     return 0;
 }
 
 int cmd_echo(int argc, char *argv[]){ 
-    fprintf(stdout, "%s\n", "cmd_echo");
+    fprintf(stdout, "%s\n", join(argv, " ")); 
     return 0;
 }
 
@@ -136,4 +166,57 @@ void memory_free(char line[LINE_MAX +1], int *argc, char * argv[]){
 void print_error(char line[LINE_MAX +1], int *argc, char * argv[]){ 
     fprintf(stderr, "Error running command .. %s\n", line); 
     return;
+}
+
+
+void print_boolean(boolean bool){ 
+    switch (bool) {
+        case false:
+            fprintf(stdout, "%s\n", "false");
+            break;
+        case true:
+            fprintf(stdout, "%s\n", "true");
+        default:
+            assert(0); 
+    }
+    return;
+}
+
+
+time_t minutes(time_t seconds){ 
+    return   seconds/60; 
+}
+
+time_t hours(time_t seconds){ 
+    return minutes(seconds)/60; 
+}
+
+time_t days(time_t seconds){
+    return hours(seconds)/24; 
+}
+
+/*functions.  Keep in mind that some years are leap years and others
+are not.  Leap years contain 366 days (February 29th) and all other
+years contain 365 days.  Every year that is evenly divisible by
+four is a leap year, except that every year divisible by 100 is not
+a leap year, except that every year divisible by 400 is a leap
+year.
+
+assert(is_leap_year(1900) == false); 
+assert(is_leap_year(2011) == false); 
+assert(is_leap_year(2000) == true); 
+*/
+
+boolean is_leap_year(int yearno){ 
+    if (yearno % 4 == 0 ){ 
+        if (yearno % 100 == 0){ 
+            if (yearno % 400 == 0) { 
+                return true; 
+            }
+            return false; 
+        } 
+        return true; 
+    } else { 
+        return false; 
+    }
 }
